be7ef3058336b35484ae49df572acdda524bf797
eguruplus <40783268+eguruplus@users.noreply.github.com>
2018-07-03 09:44:47 +0530

    Add files via upload

---
+++ ceaser.java
@@ -0,0 +1,47 @@
+class ceaser
+{
+
+    static String encrypt(String pt, int key) 
+    {
+               String ct="";
+
+        for (int i = 0; i < pt.length(); i++)
+        {
+
+            char letter = pt.charAt(i);
+            letter = (char) (letter + key);
+            if (letter > 'z') 
+            {
+                letter = (char) (letter - 26);
+            } 
+            ct+= letter;
+        }
+       return(ct);
+    }
+
+
+static String decrypt(String ct, int key) 
+{
+       String pt="";
+        for (int i = 0; i < ct.length(); i++) 
+        {
+           char letter = ct.charAt(i);
+            letter = (char) (letter - key);
+            if (letter < 'a') 
+            {
+                letter = (char) (letter + 26);
+            }
+            pt+=letter;
+        }
+        return(ct);
+    }
+    public static void main(String[] args) {
+
+        String a = "guru";
+        System.out.println(a);
+        String ct = encrypt(a, 1);
+        String pt = decrypt(ct, 1);
+        System.out.println("cipher text="+ct);
+        System.out.println("plain text="+pt);
+    }
+}
