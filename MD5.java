f1578be476adbeaf4d1f44f918fa00d31bbf5a43
eguruplus <40783268+eguruplus@users.noreply.github.com>
2018-08-28 10:34:06 +0530

    Update MD5.java

--- MD5.java
+++ MD5.java
@@ -23,6 +23,6 @@ public class MD5 {
     }
  
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        System.out.println(MD5("karthik"));
+        System.out.println(MD5("HelloWorld"));
     }
 }
